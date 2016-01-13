ab81be2c474e829a48e52e29cee738698697ea6e
zyfjeff <445188383@qq.com>
2016-01-13 10:24:52 +0800

    py implement

--- 2016-01-12_Search_in_Rotated_Sorted_Array.py
+++ 2016-01-12_Search_in_Rotated_Sorted_Array.py
@@ -8,8 +8,26 @@ class Solution(object):
         :type target: int
         :rtype: int
         """
-
-
+        first = 0
+        last = len(nums)
+        while first != last:
+            mid = (first + last) / 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] >= nums[first]:    #左侧连续
+                if target < nums[mid] and target >= nums[first]:
+                    last = mid
+                else:
+                    first = mid + 1
+            else:                           #右侧连续
+                if target > nums[mid] and target <= nums[last - 1]:
+                    first = mid + 1
+                else:
+                    last = mid
+        return -1
 
 if __name__ == "__main__":
-
+    so = Solution()
+    l = [7,8,9,1,2,3,4]
+    assert so.search(l,4) == 6
+    assert so.search(l,5) == -1
