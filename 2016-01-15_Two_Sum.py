75ea4caaf63c92e18784a6bccc50a162aeea62cf
zyfjeff <445188383@qq.com>
2016-01-18 16:28:18 +0800

    py implement

--- 2016-01-15_Two_Sum.py
+++ 2016-01-15_Two_Sum.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        data_map = {}
+        for num in xrange(len(nums)):
+            dst = target - nums[num]
+            if data_map.has_key(nums[num]):
+                return [data_map[nums[num]],num+1]
+            else:
+                data_map[dst] = num + 1
+
+
 
 if __name__ == "__main__":
-
+    num = [2,7,11,15]
+    so = Solution()
+    print(so.twoSum(num,13))
