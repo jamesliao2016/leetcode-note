163bdb0f2ba47d55df0980ef0337ee93a6e21b89
zyfjeff <445188383@qq.com>
2016-01-22 09:59:47 +0800

    01/21 task not complement

---
+++ 2016-01-21_Permutation_Sequence.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+/*
+ *  可以调用next_permulation，调用k-1次就ok了,但是效率明显不高．
+ *
+ *
+ *
+ */
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+
+        while(k--) {
+
+        }
+    }
+};
+
+int main()
+{
+
+}
