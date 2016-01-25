39fd64592d057924e1563be19f1b369cd21c8826
zyfjeff <445188383@qq.com>
2016-01-25 20:10:09 +0800

    01/25 task append

---
+++ 2016-01-25_Trapping_Rain_Water.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <vector>
+#include <map>
+#include <unordered_map>
+
+using namespace std;
+
+/*
+ *  这一题比较好玩，主要是因为题目挺有意思，题目给出一组数字，每个数字代表一个柱子的高度，
+ *  问这个柱子之间可以存放多少雨水．
+ *
+ */
+
+class Solution {
+public:
+    int trap(vector<int>& height) {
+
+    }
+};
+int main()
+{
+
+}
