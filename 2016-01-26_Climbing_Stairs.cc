febdf5565c51ccce992e598603bf9fdcd8959450
zyfjeff <445188383@qq.com>
2016-01-27 15:39:46 +0800

    1/26 第二种解法实现

--- 2016-01-26_Climbing_Stairs.cc
+++ 2016-01-26_Climbing_Stairs.cc
@@ -19,9 +19,23 @@ public:
     }
 };
 
+/*
+ *  迭代求解
+ *
+ *
+ */
 class Solution2 {
 public:
     int climbStairs(int n) {
+        int data[n + 1];  //用于迭代求解
+        //初始化迭代的基础解
+        data[1] = 1;
+        data[2] = 2;
+
+        for(int i = 3; i <= n; i++) {
+                data[i] = data[i -1] + data[i - 2];
+        }
+        return data[n];
 
     }
 };
@@ -29,6 +43,8 @@ public:
 
 int main()
 {
+    Solution2 so2;
     Solution so;
-    cout << so.climbStairs(10);
+    cout << so.climbStairs(10) << endl;
+    cout << so2.climbStairs(10);
 }
